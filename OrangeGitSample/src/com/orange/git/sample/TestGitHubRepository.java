2824dea9d7c1235d9aa0d19fc2fd4935b9c771aa
1 <1@10.10.106.13>
2014-03-23 15:04:22 +0800

    commit to local repository

--- OrangeGitSample/src/com/orange/git/sample/TestGitHubRepository.java
+++ OrangeGitSample/src/com/orange/git/sample/TestGitHubRepository.java
@@ -3,5 +3,6 @@ package com.orange.git.sample;
 public class TestGitHubRepository {
 	public static void main(String[] args) {
 		System.out.println("Hello Git and GitHub!");
+		System.out.println("test commit to local repository");
 	}
 }
