d213a5a52663e5954ad184d994f46baf5555c626
1 <1@10.10.106.25>
2014-03-31 11:05:22 +0800

    new branch and change something

--- OrangeGitSample/src/com/orange/git/sample/TestGitHubRepository.java
+++ OrangeGitSample/src/com/orange/git/sample/TestGitHubRepository.java
@@ -4,5 +4,6 @@ public class TestGitHubRepository {
 	public static void main(String[] args) {
 		System.out.println("Hello Git and GitHub!");
 		System.out.println("test commit to local repository");
+		System.out.println("I am a new branch.");
 	}
 }
