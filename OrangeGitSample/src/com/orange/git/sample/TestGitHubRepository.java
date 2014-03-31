260f2c9a5102cd731d91984857a787435f36fea9
1 <1@10.10.106.25>
2014-03-31 11:09:54 +0800

    add master change which branches want to merge

--- OrangeGitSample/src/com/orange/git/sample/TestGitHubRepository.java
+++ OrangeGitSample/src/com/orange/git/sample/TestGitHubRepository.java
@@ -5,4 +5,8 @@ public class TestGitHubRepository {
 		System.out.println("Hello Git and GitHub!");
 		System.out.println("test commit to local repository");
 	}
+	
+	public static void masterChanges(){
+		System.out.println("Here is the changes on master which maybe your new branch want.");
+	}
 }
